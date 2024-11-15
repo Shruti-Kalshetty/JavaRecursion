223a24ed2f06206bef30fba6e931c960e1fe1388
Shruti Rajkumar Kalshetty <shrutikalshetty07@gmail.com>
2024-11-15 22:51:00 +0530

    Add files via upload

---
+++ SimpleMethodCall.java
@@ -0,0 +1,12 @@
+public class SimpleMethodCall {
+    
+    // Method that prints a message
+    public static void sayHello() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        // Calling the method sayHello
+        sayHello();
+    }
+}
